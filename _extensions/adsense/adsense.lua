010c522603d0270ca21dd1658194b5131738ab9e
Je Sian Keith Herman <68434444+jsk-herman@users.noreply.github.com>
2022-09-21 19:33:03 +0800

    Update adsense extension

--- _extensions/adsense/adsense.lua
+++ _extensions/adsense/adsense.lua
@@ -1,18 +1,18 @@
-function adsense(args, kwargs, meta)
+function adsense(args, meta)
     -- adsense creds vars
     local inArticleSlot = pandoc.utils.stringify(args[1])
     local client = pandoc.utils.stringify(meta["adsense"])
 
     -- Assemble script to be returned
-    local adsense = '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>'
+    local adsense = '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" crossorigin="anonymous"></script>\n'
         ..
-        '<ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article" data-ad-format="fluid"'
+        '<ins class="adsbygoogle"\n style="display:block; text-align:center;"\n data-ad-layout="in-article"\n data-ad-format="fluid"\n '
         .. 'data-ad-client="'
         .. client
-        .. '" data-ad-slot="'
+        .. '"\n data-ad-slot="'
         .. inArticleSlot
-        .. '"></ins>'
-        .. '<script>(adsbygoogle = window.adsbygoogle || []).push({});</script>'
+        .. '"></ins>\n '
+        .. '<script>(adsbygoogle = window.adsbygoogle || []).push({});</script>\n'
 
     if quarto.doc.isFormat('html') then
         return pandoc.RawBlock('html', adsense)
